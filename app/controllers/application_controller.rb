c6c91cb1716d1b2951cd71a174a2085f28d89070
ruvazrael <marco.wittich@metaminded.com>
2011-11-15 17:03:31 +0100

    authentcation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def self.require_authentication()
+    before_filter :authenticate
+  end
+
   private
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
+
   helper_method :current_user
+  
+  def authenticate() 
+    if !current_user
+      flash.alert = "You need to be logged in!"
+      redirect_to login_path
+    end
+  end
+  
 end    
